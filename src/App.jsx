0f3adbb9219f45a69d63b5b93594d540418270ae
Wakana <wakaichi@gmail.com>
2023-05-05 14:52:50 +0400

    feat: add axios to get data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [data, setData] = useState('');
+  const [id, setID] = useState('');
+  
+  const getData = async () =>{
+    try{
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      setData(response.data.slip.advice);
+      setID(response.data.slip.id);
+    } catch (error){
+      console.log(error.response);
+    }
+  }
+
+getData();
   return (
     <>
-      
+    <div className='box'>
+      <p>ADVICE #{id}</p>
+      <h1>"{data}"</h1>
+    </div>
     </>
   )
 }
