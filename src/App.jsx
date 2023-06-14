4a0fddfced3256490c2d8be056dd13ad29c3efd0
Wakana <wakanichi13@gmail.com>
2023-06-14 17:06:15 +0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 import './App.css'
-
+import mobileSVG from "./images/pattern-divider-mobile.svg";
+import desktopSVG from "./images/pattern-divider-desktop.svg";
 
 function App() {
 const [response, setResponse] = useState({slip: {} });
@@ -22,8 +23,9 @@ useEffect(() => {
     <div className='box'>
       <p>ADVICE #{response.slip.id}</p>
       <h1>"{response.slip.advice}"</h1>
-      <img className='mobileSVG' src="./src/images/pattern-divider-mobile.svg" alt="" />
-      <img className='desktopSVG' src="./src/images/pattern-divider-desktop.svg" alt="" />
+      <img className='mobileSVG' src={mobileSVG} alt="" />
+      <img className='desktopSVG' src={desktopSVG} alt="" />
+      
     </div>
     <div className="circle" onClick={getData}>
       <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
